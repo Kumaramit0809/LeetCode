ed43ee6fa08112d27c3b9f79d3bd47aaed5dbfe2
AMIT KUMAR <126111875+Kumaramit0809@users.noreply.github.com>
2025-04-06 01:16:40 +0530

    Time: 0 ms (100%), Space: 29.4 MB (79.82%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_set<int> seen;
+        int currentSum = 0;
+        int maxElement = nums[0];  // Since nums is guaranteed to be non-empty
+
+        for (int num : nums) {
+            maxElement = max(maxElement, num);
+            if (num > 0 && !seen.count(num)) {
+                currentSum += num;
+                seen.insert(num);
+            }
+        }
+
+        return currentSum == 0 ? maxElement : currentSum;
+    }
+};
