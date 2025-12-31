23c0c17a054f88a35287e078ebe74a6e4a8ad7a7
AMIT KUMAR <126111875+Kumaramit0809@users.noreply.github.com>
2025-12-31 17:42:19 +0530

    Time: 7 ms (63.23%), Space: 21.5 MB (51.89%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        freq = {}
+        result = []
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+             
+
+        for key, value in freq.items():
+            if value > n // 3:
+                result.append(key)
+
+        return result
